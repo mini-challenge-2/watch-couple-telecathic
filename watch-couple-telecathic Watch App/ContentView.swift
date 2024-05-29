9adb3b9009e0e3f2bd0dd12713df61b5069f43e8
ssabrut <michael.gunawan2002@gmail.com>
2024-05-29 09:33:01 +0700

    Initial Commit

---
+++ watch-couple-telecathic Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watch-couple-telecathic Watch App
+//
+//  Created by Michael Eko on 29/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watch-couple-telecathic Watch App/watch_couple_telecathicApp.swift
@@ -0,0 +1,17 @@
+//
+//  watch_couple_telecathicApp.swift
+//  watch-couple-telecathic Watch App
+//
+//  Created by Michael Eko on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct watch_couple_telecathic_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
